dc1ce3ca013b03c2685bfeddece10ba8a2eecba8
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-22 11:59:13 -0800

    no more props inside of my component

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 
-// const ImageDetail = ({imageSoure, title, imageScore}) => {
-const ImageDetail = (props) => {
+// const ImageDetail = (props) => {
+const ImageDetail = ({ imageSoure, title, imageScore }) => {
     // console.log(props);
     return(
         <View>
             {/* <Image source={require('../../assets/beach.jpg')} /> */}
-            {/* <Image source={imageSoure} />
-            <Text>{title}</Text>
-            <Text>Image score - {imageScore}</Text> */}
-            <Image source={props.imageSoure} />
+            {/* <Image source={props.imageSoure} />
             <Text>{props.title}</Text>
-            <Text>Image score - {props.imageScore}</Text>
+            <Text>Image score - {props.imageScore}</Text> */}
+            <Image source={imageSoure} />
+            <Text>{title}</Text>
+            <Text>Image score - {imageScore}</Text>
         </View>
     );
 };
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,7 +5,10 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title="Forest" imageScore={9} imageSoure={require('../../assets/forest.jpg')} />
+            <ImageDetail 
+            title="Forest" 
+            imageScore={9} 
+            imageSoure={require('../../assets/forest.jpg')} />
             <ImageDetail title="Beach" imageScore={7} imageSoure={require('../../assets/beach.jpg')}/>
             <ImageDetail title="Mountain" imageScore={4} imageSoure={require('../../assets/mountain.jpg')}/>
         </View>
