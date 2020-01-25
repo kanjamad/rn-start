6f45ea5d4a578df5ff0d0cfecd71ac26f11f9ddc
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-24 18:02:54 -0800

    Tying state Value together

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { View,Text, StyleSheet, Button} from 'react-native';
 
 // const ColorCounter = (porps) => {
-const ColorCounter = ({color}) => {
+const ColorCounter = ({color, onIncrease, onDecrease}) => {
 
     return ( 
         <View >
         <Text > {color} </Text> 
         <Button 
         title={`Increate Red ${color}`}
+        onPress={()=>onIncrease()}
         />
         < Button
         title = {`Decreate Red ${color}` }
+        onPress={()=>onDecrease()}
         />
         </View>
     );
--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,12 +6,31 @@ const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
-
+    // console.log(red);
+    // console.log(green);
+    // console.log(blue);
     return (
         <View>
-            <ColorCounter color="Red"/>
-            <ColorCounter color="Blue"/>
-            <ColorCounter color="Green"/>
+            <ColorCounter 
+            onIncrease={() =>setRed(red +15)} 
+            onDecrease={() =>setRed(red -15)} 
+            color="Red"
+            />
+            <ColorCounter 
+            onIncrease = { () => setGreen(green +15)}
+            onDecrease = { () => setGreen(green -15)}
+            color="Green"
+            />
+            <ColorCounter 
+            onIncrease = { () => setBlue(blue +15)}
+            onDecrease = { () => setBlue(blue -15)}
+            color="Blue"
+            />
+            <View style={{
+                height:150,
+                width:150,
+                backgroundColor: `rgb(${red},${green},${blue})`
+            }} />
         </View>
     );
 };
