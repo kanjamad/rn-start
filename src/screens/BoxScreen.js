72110271b29c6681fd0d2174a21e04155e579b74
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-30 12:29:56 -0800

    Add some styles

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,13 +3,21 @@ import {Text, View,StyleSheet} from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text> Hi Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyle}> Hi Box Screen</Text>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    viewStyle:{
+        borderWidth:3,
+        borderColor:'black',
+    },
+    textStyle:{
+        borderWidth:1,
+        borderColor:'red'
+    },
 
 });
 
