fe04d25c1132f1ffe2ac792a4401ad8cf05fa87e
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-30 11:54:47 -0800

    Add boilerplate

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -42,6 +42,11 @@ const HomeScreen = ({navigation}) => {
       onPress={()=>navigation.navigate('Text')}
       />
 
+      <Button 
+      title="Go to Box Demo"
+      onPress={()=>navigation.navigate('Box')}
+      />
+
     </View>
   );
 };
