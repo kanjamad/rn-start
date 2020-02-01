d303a8db31a9d40f74903d6a000741de06964eb4
kanjamad bosel <kanjamadpor@gmail.com>
2020-01-31 19:27:29 -0800

    add justifyContent

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -15,9 +15,8 @@ const styles = StyleSheet.create({
     viewStyle:{
         borderWidth:3,
         borderColor:'black',
-        flexDirection:'row',
         height: 200,
-        alignItems:'center',
+        justifyContent:'space-between',
     },
     textStyle:{
         borderWidth:2,
